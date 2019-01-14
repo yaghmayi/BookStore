598962f33a73cbd030e47dd64fdfc80743c5eec4
yaghmayi <yaghmayi@gmail.com>
2019-01-14 15:32:57 +0100

    Fix AddSopItem javascript

--- Web/Content/Scripts/AddShopItem.js
+++ Web/Content/Scripts/AddShopItem.js
@@ -3,12 +3,13 @@
     $.ajax({
         url: '/Book/AddShopItem/',
         data: { id: bookCode },
+        complete: function (jqXHR, status) {
+            var spanShopItemsCount = $("#shopItemsCount");
+            spanShopItemsCount.html(GetSession("ShopItemsCount"));
+
+            var progressBar = $("#prg" + bookCode);
+            progressBar.fadeIn(1000);
+            progressBar.fadeOut(3000);
+        }
     });
-        
-    var spanShopItemsCount = $("#shopItemsCount");
-    spanShopItemsCount.html(GetSession("ShopItemsCount"));
-        
-    var progressBar = $("#prg" + bookCode);
-    progressBar.fadeIn(1000);
-    progressBar.fadeOut(3000);
 }
